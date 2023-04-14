2094cc31360de103688a7efa328a63e5b099d104
DanielFlanagan05 <danflanman@icloud.com>
2023-04-14 17:28:04 -0400

    Fixed errors in server.js and script.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+// Serve index.html on root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 // Middleware
 app.use(express.static(__dirname));
@@ -19,6 +26,10 @@ app.get('/getClicks', (req, res) => {
 app.get('/increment', (req, res) => {
     clickCount++;
     res.json({ clicks: clickCount });
+});
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/public/style.css', { headers: { 'Content-Type': 'text/css' } });
 });
 
 // Start server
